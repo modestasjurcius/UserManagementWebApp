86fbe02905397bac658b96272136e59b524696f3
modestasjurcius <modesjur@gmail.com>
2019-12-24 00:20:14 +0200

    Data: added UserManagementDbContext class.

---
+++ UserManagementWebApp/Data/UserManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagementWebApp.Models
+{
+    public class UserManagementDbContext : DbContext
+    {
+        public UserManagementDbContext (DbContextOptions<UserManagementDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserManagementWebApp.Models.User> User { get; set; }
+    }
+}
