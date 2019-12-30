94712d47f8a184e38224edd215ec9f7440e3c9a0
modestasjurcius <modesjur@gmail.com>
2019-12-31 00:06:41 +0200

    Models/SeedData: filled in seed data with new columns.

--- UserManagementWebApp/Models/SeedData.cs
+++ UserManagementWebApp/Models/SeedData.cs
@@ -26,27 +26,36 @@ namespace UserManagementWebApp.Models
                 (
                     new User
                     {
+                        Username = "john",
+                        Password = "john",
                         FirstName = "John",
                         LastName = "Vick",
                         Sex = "Male",
                         BirthDate = DateTime.Parse("1964-9-2"),
-                        Description = "Basically, John was one of the greatest assassins who ever lived. He was so deadly that he earned the nickname Baba Yaga, which means The Boogeyman."
+                        Description = "Basically, John was one of the greatest assassins who ever lived. He was so deadly that he earned the nickname Baba Yaga, which means The Boogeyman.",
+                        RegistrationDate = DateTime.Parse("2019-12-31")
                     },
                     new User
                     {
+                        Username = "modestas",
+                        Password = "modestas",
                         FirstName = "Modestas",
                         LastName = "Jurčius",
                         Sex = "Male",
                         BirthDate = DateTime.Parse("1998-5-12"),
-                        Description = "A software engineering student that loves to do some coding."
+                        Description = "A software engineering student who loves to do some coding magic.",
+                        RegistrationDate = DateTime.Parse("2019-12-31")
                     },
                     new User
                     {
+                        Username = "dalia",
+                        Password = "dalia",
                         FirstName = "Dalia",
                         LastName = "Grybauskaitė",
                         Sex = "Female",
                         BirthDate = DateTime.Parse("1956-3-1"),
-                        Description = "Lithuanian politician who served as the eighth President of Lithuania from 2009 until 2019."
+                        Description = "Lithuanian politician who served as the eighth President of Lithuania from 2009 until 2019.",
+                        RegistrationDate = DateTime.Parse("2019-12-31")
                     }
                 );
                 context.SaveChanges();
