beaed74e33ff1e20b2cb3f6e558e40610e3a309c
modestasjurcius <modesjur@gmail.com>
2020-01-02 14:01:43 +0200

    Models/SeedData: added more items to seed data.

--- UserManagementWebApp/Models/SeedData.cs
+++ UserManagementWebApp/Models/SeedData.cs
@@ -56,6 +56,28 @@ namespace UserManagementWebApp.Models
                         BirthDate = DateTime.Parse("1956-3-1"),
                         Description = "Lithuanian politician who served as the eighth President of Lithuania from 2009 until 2019.",
                         RegistrationDate = DateTime.Parse("2019-12-31")
+                    },
+                    new User
+                    {
+                        Username = "johnnny",
+                        Password = "the_beatles",
+                        FirstName = "John",
+                        LastName = "Lennon",
+                        Sex = "Male",
+                        BirthDate = DateTime.Parse("1940-10-9"),
+                        Description = "John Lennon was an English singer, songwriter and peace activist who gained worldwide fame as the founder, co-lead vocalist, and rhythm guitarist of the Beatles.",
+                        RegistrationDate = DateTime.Parse("2020-1-2")
+                    },
+                    new User
+                    {
+                        Username = "magnus",
+                        Password = "magnus",
+                        FirstName = "Magnus",
+                        LastName = "Carlsen",
+                        Sex = "Male",
+                        BirthDate = DateTime.Parse("1990-11-30"),
+                        Description = "Magnus Carlsen is a Norwegian chess grandmaster who is the current World Chess Champion, three-time World Rapid Chess Champion and five-time World Blitz Chess Champion.",
+                        RegistrationDate = DateTime.Parse("2020-1-2")
                     }
                 );
                 context.SaveChanges();
